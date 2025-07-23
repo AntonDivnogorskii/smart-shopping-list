be9b4a9f2711bfd209a1cc23fe602d5e78d7a2ac
Dark Phoenix <anton2004.12.24@gmail.com>
2025-07-23 16:13:34 +0300

    Добавил почти рабочий фронтенд

--- backend/src/main/java/com/example/backend/controller/NoteController.java
+++ backend/src/main/java/com/example/backend/controller/NoteController.java
@@ -13,14 +13,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/notes")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:8080")
 public class NoteController {
     private final NoteService noteService;
 
-    @GetMapping
-    public String index() {
-        return "redirect:/index.html";
-    }
-
+    //отдаём все записи
     @GetMapping
     public ResponseEntity<?> getAllNotes() {
         try {
@@ -34,27 +31,68 @@ public class NoteController {
         }
     }
 
-    @PostMapping
-    public ResponseEntity<?> getAllNotesPost() {
-        try {
-            List<Note> notes = noteService.readAllNotes();
-            return ResponseEntity.ok(notes);
-        } catch (Exception e) {
-            System.out.println("Error getting notes: " + e.getMessage());
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Error to get notes" + e.getMessage());
-        }
-    }
-
-    @PostMapping("/filter")
+    //фильтр по количеству и флагу
+    @PostMapping("/filter_especial")
     public ResponseEntity<List<Note>> filterNotes(@RequestBody NoteFilterRequest filter) {
         List<Note> devices = noteService.filterNotes(filter);
         return ResponseEntity.ok(devices);
     }
 
+    //поиск по имени
     @GetMapping("/search")
-    public ResponseEntity<List<Note>> searchDevicesByName(@RequestParam String name) {
+    public ResponseEntity<List<Note>> searchNotesByName(@RequestParam String name) {
         return ResponseEntity.ok(noteService.searchNotesByName(name));
+    }
+
+    // Добавление нового товара
+    @PostMapping
+    public ResponseEntity<?> addNote(@RequestBody Note note) {
+        try {
+            Note savedNote = noteService.saveNote(note);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedNote);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error adding note: " + e.getMessage());
+        }
+    }
+//    @PostMapping("/save_note")
+//    public ResponseEntity<?> addNote(@RequestBody Note note) {
+//        try {
+//            Note savedDevice = noteService.saveNote(note);
+//            return ResponseEntity.ok(savedDevice);
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                    .body("Error adding product: " + e.getMessage());
+//        }
+//    }
+
+    // Обновление товара
+    @PutMapping("/update_note/{id}")
+    public ResponseEntity<?> updateNote(@PathVariable Long id, @RequestBody Note note) {
+        try {
+            note.setId(id);
+            Note updatedDevice = noteService.updateNote(note);
+            return ResponseEntity.ok(updatedDevice);
+        } catch (Exception e) {
+            System.out.println("Error updating product: " + e.getMessage());
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error updating product: " + e.getMessage());
+        }
+    }
+
+    // Удаление товара
+    @DeleteMapping("/delete_note/{id}")
+    public ResponseEntity<?> deleteNote(@PathVariable Long id) {
+        try {
+            System.out.println("=== ADMIN: DELETING PRODUCT " + id + " ===");
+            noteService.deleteNote(id);
+            return ResponseEntity.ok("Product deleted successfully");
+        } catch (Exception e) {
+            System.out.println("Error deleting product: " + e.getMessage());
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error deleting product: " + e.getMessage());
+        }
     }
 }
